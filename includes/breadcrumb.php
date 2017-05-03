b07b753ee7ec6b4470d1e0b49b13d29eb67628c0
Rohit <rohit@nettrackers.net>
2017-05-03 18:13:47 +0530

    changes

---
+++ includes/breadcrumb.php
@@ -0,0 +1,13 @@
+	<!-- Breadcrumb -->				   
+	<ul class="breadcrumb" id="productsBreadcrumb">
+		<li>
+			<a data-categoryindex="all" href="#/category/uid-all" class="category-link-all category-link">All</a>
+		</li>
+		<span class="divider">/</span> 
+		<li>
+			<a data-categoryindex="all" href="#/category/uid-kids-furniture" class="category-link-kids-furniture category-link">Kids Furniture</a>
+		</li>
+		<span class="divider">/</span> 
+		<li> Rocker Dolphin </li>
+	</ul>
+	<!-- /Breadcrumb -->
--- includes/header.php
+++ includes/header.php
@@ -22,22 +22,9 @@
 						</a>
 					</span>          
 				</span>
-			<!-- /Show Cart Button --> 
+				<!-- /Show Cart Button --> 
 			</div>
 		</div>
-		
-		<!-- Breadcrumb -->				   
-		<ul class="breadcrumb" id="productsBreadcrumb">
-			<li>
-				<a data-categoryindex="all" href="#/category/uid-all" class="category-link-all category-link">All</a>
-			</li>
-			<span class="divider">/</span> 
-			<li>
-				<a data-categoryindex="all" href="#/category/uid-kids-furniture" class="category-link-kids-furniture category-link">Kids Furniture</a>
-			</li>
-			<span class="divider">/</span> 
-			<li> Rocker Dolphin </li>
-		</ul>
-		<!-- /Breadcrumb -->
+		<?php include_once("breadcrumb.php");?>		
 	</div>
 	<!--  /header -->
